1f8ed52758fadd8ef4c9db259841b091e5968360
chavdim <chavdardim90@gmail.com>
2023-05-21 01:44:51 +0900

    move logit to client class to maybe make it more testable

---
+++ client/chat_client.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	"fmt"
+	"github.com/chavdim/gollm/drivers"
+	"github.com/chavdim/gollm/util"
+	"github.com/sashabaranov/go-openai"
+)
+
+const interactionLimit = 5
+
+type ChatClient struct {
+	client         *openai.Client
+	messageHistory []drivers.ChatCompletionRequest
+}
+
+func (c *ChatClient) InitClient(secretKey string) {
+	c.client = openai.NewClient(secretKey)
+}
+
+func (c *ChatClient) StartChatLoop(initialPrompt string) {
+	var i = 0
+	for {
+		i++
+		fmt.Printf("### interaction: %d\n", i)
+		var prompt drivers.ChatCompletionRequest
+		if len(initialPrompt) > 0 && i == 1 {
+			prompt = drivers.ChatCompletionRequest{Prompt: initialPrompt}
+		} else {
+			fmt.Print("Enter prompt: ")
+			prompt = util.GetInputOrErr()
+		}
+
+		c.messageHistory = append(c.messageHistory, prompt)
+		drivers.OpenAIStreamedResponse(c.client, c.messageHistory)
+
+		// for safety, limit interactions
+		if i == interactionLimit {
+			break
+		}
+	}
+}
--- gollm.go
+++ gollm.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"github.com/chavdim/gollm/drivers"
-	"github.com/chavdim/gollm/util"
-	openai "github.com/sashabaranov/go-openai"
+	"github.com/chavdim/gollm/client"
 	"os"
 	"strings"
 )
 
 func main() {
+	var secretKey = attainApiKey()
+	var initialPrompt = getInitialPrompt()
+	var chatClient = client.ChatClient{}
+	chatClient.InitClient(secretKey)
+	chatClient.StartChatLoop(initialPrompt)
+}
+
+func getInitialPrompt() string {
 	var initialPrompt = ""
 	if len(os.Args) > 1 {
 		initialPrompt = strings.Join(os.Args, " ")
 	}
+	return initialPrompt
+}
 
+func attainApiKey() string {
 	var secretKey, exists = os.LookupEnv("OPENAI_API_KEY")
 	if !exists {
 		panic("Key not found. Set OPENAI_API_KEY env var see https://platform.openai.com/account/api-keys")
 	}
-	client := openai.NewClient(secretKey)
-
-	var i = 0
-	var prompts []drivers.ChatCompletionRequest
-	for {
-		i++
-		fmt.Printf("### interaction: %d\n", i)
-		var prompt drivers.ChatCompletionRequest
-		if len(initialPrompt) > 0 && i == 1 {
-			prompt = drivers.ChatCompletionRequest{Prompt: initialPrompt}
-		} else {
-			fmt.Print("Enter prompt: ")
-			prompt = util.GetInputOrErr()
-		}
-
-		prompts = append(prompts, prompt)
-		drivers.OpenAIStreamedResponse(client, prompts)
-
-		// for safety, limit interactions
-		if i == 5 {
-			break
-		}
-	}
-
+	return secretKey
 }
